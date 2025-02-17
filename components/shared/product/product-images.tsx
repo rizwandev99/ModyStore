803829587fbb76f616560179c6327c7a15423da9
Md Rizwan <rizwandev99@gmail.com>
2025-02-17 15:16:20 +0530

    fixed the error

--- components/shared/product/product-images.tsx
+++ components/shared/product/product-images.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -15,9 +14,7 @@ const ProductImages = ({ images }: { images: string[] }) => {
         width={1000}
         className="min-h-[300px] object-cover object-center"
       />
-      <div className="flex "
-      
-      >
+      <div className="flex ">
         {images.map((image, index) => (
           <Image
             key={index}
