e9ab81836fe218c4d771b03e8daff87865e96d24
Md Rizwan <rizwandev99@gmail.com>
2025-02-27 15:10:12 +0530

    finished signin form

---
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { signInDefaultValues } from "@/lib/constants";
+import Link from "next/link";
+
+const CredentialsSignInForm = () => {
+  return (
+    <form>
+      <div className="space-y-6">
+        <div>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            required
+            autoComplete="email"
+            defaultValue={signInDefaultValues.email}
+          />
+        </div>
+        <div>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            required
+            autoComplete="password"
+            defaultValue={signInDefaultValues.password}
+          />
+        </div>
+        <div>
+          <Button className="w-full" variant={"default"}>
+            Sign In
+          </Button>
+        </div>
+        <div>
+          Don&apos;t have an account{" "}
+          <Link href="/sign-up" target="_self" className="link">
+            Sign up
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CredentialsSignInForm;
