1208e4e6eeff90d45a9d6b31fc7ecc78e8e6d1c1
Md Rizwan <rizwandev99@gmail.com>
2025-03-20 15:32:10 +0530

    Updated validators and added shipping address schema; improved consistency in string quotes across files.

---
+++ app/(root)/shipping-address/page.tsx
@@ -0,0 +1,24 @@
+import { auth } from "@/auth";
+import { getMyCart } from "@/lib/actions/cart.actions";
+import { getUserById } from "@/lib/actions/user.actions";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Shipping Address",
+};
+
+const ShippingAddressPage = async () => {
+  const cart = await getMyCart();
+  if (!cart || cart.items.length === 0) redirect("/cart");
+
+  const session = await auth();
+  const userId = session?.user?.id;
+  
+  if (!userId) throw new Error("No User found");
+  const user = getUserById(userId);
+
+  return <>Address</>;
+};
+
+export default ShippingAddressPage;
