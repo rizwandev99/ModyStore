5d06245f0e5ad19eac051bc8f996076569688ce2
Md Rizwan <rizwandev99@gmail.com>
2025-03-25 14:10:30 +0530

    payment method - radio button done

---
+++ app/(root)/payment-method/payment-method-form.tsx
@@ -0,0 +1,106 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useToast } from "@/hooks/use-toast";
+import { DEFAULT_PAYMENT_METHOD, PAYMENT_METHODS } from "@/lib/constants";
+import { paymentMethodSchema } from "@/lib/validators";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Loader } from "lucide-react";
+import { z } from "zod";
+
+const PaymentMethodForm = ({
+  preferredPaymentMethod,
+}: {
+  preferredPaymentMethod: string | null;
+}) => {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof paymentMethodSchema>>({
+    resolver: zodResolver(paymentMethodSchema),
+    defaultValues: preferredPaymentMethod || DEFAULT_PAYMENT_METHOD,
+  });
+
+  const [isPending, startTransition] = useTransition();
+
+  const onSubmit = () => {
+    return;
+  };
+
+  return (
+    <>
+      <div className="max-w-md mx-auto space-y-4">
+        <h1 className="h2-bold mt-4">Payment Method</h1>
+        <p className="text-sm text-muted-foreground">
+          Please select a payment method
+        </p>
+
+        <Form {...form}>
+          <form
+            method="post"
+            className="space-y-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <div className="flex flex-col md:flex-row gap-5">
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem className="space-y-3">
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        className="flex flex-col space-y-2"
+                      >
+                        {PAYMENT_METHODS.map(({ paymentMethod }) => (
+                          <FormItem
+                            key={paymentMethod}
+                            className="flex items-center space-x-3 space-y-0"
+                          >
+                            <FormControl>
+                              <RadioGroupItem
+                                value={paymentMethod}
+                                checked={field.value === paymentMethod}
+                              />
+                            </FormControl>
+                            <FormLabel className="font-normal">
+                              {paymentMethod}
+                            </FormLabel>
+                          </FormItem>
+                        ))}
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="flex gap-2">
+              <Button type="submit" disabled={isPending}>
+                {isPending ? (
+                  <Loader className="h-4 w-4 animate-spin" />
+                ) : (
+                  <ArrowRight className="h-4 w-4" />
+                )}
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default PaymentMethodForm;
