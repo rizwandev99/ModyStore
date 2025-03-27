c206cdab48d0dd4329d8599ef1ee567f434f9ba7
Md Rizwan <rizwandev99@gmail.com>
2025-03-27 17:55:40 +0530

    place order ui page done

--- auth.ts
+++ auth.ts
@@ -69,7 +69,7 @@ export const config = {
 
       return session;
     },
-    async jwt({ token, user, trigger, session }: any) {
+    async jwt({ token, user, trigger,  }: any) {
       // Assign user fields to token
       if (user) {
         token.role = user.role;
