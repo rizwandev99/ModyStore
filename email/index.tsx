98369d3ff48d12e6a7a73ac4b86184f73219a6bc
Md Rizwan <rizwandev99@gmail.com>
2025-07-09 13:10:49 +0530

    deployment attempt -2

--- email/index.tsx
+++ email/index.tsx
@@ -1,9 +1,6 @@
 import { Resend } from 'resend';
 import { SENDER_EMAIL, APP_NAME } from '@/lib/constants';
 import { Order } from '@/types';
-import dotenv from 'dotenv';
-dotenv.config();
-
 import PurchaseReceiptEmail from './purchase-receipt';
 
 const resend = new Resend(process.env.RESEND_API_KEY as string);
